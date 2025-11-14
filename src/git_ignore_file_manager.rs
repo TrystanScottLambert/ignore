05682399e58671000715c875c1f21acdb055893f
TrystanScottLambert <trystanscottlambert@gmail.com>
2025-11-14 18:29:34 +0800

    formatting

--- src/git_ignore_file_manager.rs
+++ src/git_ignore_file_manager.rs
@@ -1,19 +1,19 @@
 /// Module for finding appending and writing gitignore files.
-use std::{fmt::format, fs};
+use std::fs;
 
-enum IgnoreType {
-    PYTHON,
-    RUST,
+pub enum IgnoreType {
+    Python,
+    Rust,
     R,
-    CUSTOM,
+    Custom,
 }
 /// Reads the git ignore .dat files in data/
 fn read_ignore_template(which: IgnoreType) -> String {
     let template_file = match which {
-        IgnoreType::PYTHON => "./src/data/python.dat",
-        IgnoreType::RUST => "./src/data/rust.dat",
+        IgnoreType::Python => "./src/data/python.dat",
+        IgnoreType::Rust => "./src/data/rust.dat",
         IgnoreType::R => "./src/data/r.dat",
-        IgnoreType::CUSTOM => "./src/data/my_own.dat",
+        IgnoreType::Custom => "./src/data/my_own.dat",
     };
     fs::read_to_string(template_file).expect("Core tempalates not found.")
 }
@@ -24,10 +24,10 @@ mod tests {
 
     #[test]
     fn test_reading_template_files() {
-        let rust_string = read_ignore_template(IgnoreType::RUST);
-        let python_string = read_ignore_template(IgnoreType::PYTHON);
+        let rust_string = read_ignore_template(IgnoreType::Rust);
+        let python_string = read_ignore_template(IgnoreType::Python);
         let r_string = read_ignore_template(IgnoreType::R);
-        let own_string = read_ignore_template(IgnoreType::CUSTOM);
+        let own_string = read_ignore_template(IgnoreType::Custom);
         println!("{rust_string}");
         println!("{python_string}");
         println!("{r_string}");
